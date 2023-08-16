d7d71486d9add64666f72d24562b22354dc4f427
FindMeSomeFun <findmesomefun@gmail.com>
2023-08-16 09:48:26 +0200

    Three-headed giraffe Hydra.

--- src/main/java/betterquesting/api2/client/gui/panels/content/PanelEntityPreview.java
+++ src/main/java/betterquesting/api2/client/gui/panels/content/PanelEntityPreview.java
@@ -114,7 +114,9 @@ public class PanelEntityPreview implements IGuiPanel
 		} else if (EntityList.getEntityString(entity).equals("TwilightForest.Hydra")) {
 			pitch = pitchDriver.readValue();
 			Entity head = EntityList.createEntityByName("TwilightForest.HydraHead", Minecraft.getMinecraft().theWorld);
-			RenderUtils.RenderEntity(bounds.getX() + sizeX/2, (int) (bounds.getY() + sizeY/2 + head.height * scale), (int)scale, yawDriver.readValue(), pitch, head);
+			RenderUtils.RenderEntity(bounds.getX() + sizeX/2, (int) (bounds.getY() + sizeY/2 - head.height * scale), (int)scale, yawDriver.readValue(), pitch, head);
+			RenderUtils.RenderEntity(bounds.getX() + sizeX/2, (int) (bounds.getY() + sizeY/2 + head.height*.25 * scale), (int)scale, yawDriver.readValue(), pitch, head);
+			RenderUtils.RenderEntity(bounds.getX() + sizeX/2, (int) (bounds.getY() + sizeY/2 + head.height*1.5 * scale), (int)scale, yawDriver.readValue(), pitch, head);
 		} else
 			pitch = pitchDriver.readValue();
 
