51f3c6d759b8725847cea8da272ede082f8bf8a0
FindMeSomeFun <findmesomefun@gmail.com>
2023-08-23 21:33:27 +0200

    NOTE: Hydra's heads still shaking in preview. Best seen in Select mob preview.

--- src/main/java/betterquesting/api/utils/RenderUtils.java
+++ src/main/java/betterquesting/api/utils/RenderUtils.java
@@ -142,22 +142,19 @@ public class RenderUtils {
                 int bodySize = 11;    // 0 to 12
                 Entity part = entity.getParts()[0];
                 float [][] xyzYaw = getNagaXyzYaw();
-                for (int i = 0; i < bodySize; i++) {
+                for (int i = 0; i < bodySize; i++)
                     RenderManager.instance.renderEntityWithPosYaw(part, xyzYaw[i][0], xyzYaw[i][1], xyzYaw[i][2], xyzYaw[i][3], 1F);
-                }
             } else if (EntityList.getEntityString(entity).equals("TwilightForest.Hydra")) {
                 OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240, 240);
                 int headsNumber = 7;    // 0 to 7, optimal 3, 5, 7
                 Entity part = EntityList.createEntityByName("TwilightForest.HydraHead", Minecraft.getMinecraft().theWorld);
                 float [][] xyz = getHydraHeadXyz();
-                for (int i = 0; i < headsNumber; i++) {
+                for (int i = 0; i < headsNumber; i++)
                     RenderManager.instance.renderEntityWithPosYaw(part, xyz[i][0], xyz[i][1], xyz[i][2], 0F, 1F);
-                }
                 part = entity.getParts()[4];
                 xyz = getHydraNeckXyz();
-                for (int i = 0; i < 5 * headsNumber; i++) {
+                for (int i = 0; i < 5 * headsNumber; i++)
                     RenderManager.instance.renderEntityWithPosYaw(part, xyz[i][0], xyz[i][1], xyz[i][2], 0F, 1F);
-                }
             }
             GL11.glDisable(GL11.GL_DEPTH_TEST);
             GL11.glPopMatrix();
