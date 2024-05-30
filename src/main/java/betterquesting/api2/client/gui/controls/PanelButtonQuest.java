22b140bd1f1127e8b30a78b76c485de653111f2a
Lyfts <127234178+Lyfts@users.noreply.github.com>
2024-05-30 20:53:33 +0200

    deal with null differently

--- src/main/java/betterquesting/api2/client/gui/controls/PanelButtonQuest.java
+++ src/main/java/betterquesting/api2/client/gui/controls/PanelButtonQuest.java
@@ -7,7 +7,6 @@ import betterquesting.api.properties.NativeProps;
 import betterquesting.api.questing.IQuest;
 import betterquesting.api.questing.tasks.ITask;
 import betterquesting.api.storage.BQ_Settings;
-import betterquesting.api.utils.BigItemStack;
 import betterquesting.api2.client.gui.misc.GuiRectangle;
 import betterquesting.api2.client.gui.misc.IGuiRect;
 import betterquesting.api2.client.gui.resources.colors.IGuiColor;
@@ -21,11 +20,12 @@ import betterquesting.api2.storage.DBEntry;
 import betterquesting.api2.utils.QuestTranslation;
 import betterquesting.client.BookmarkHandler;
 import betterquesting.questing.QuestDatabase;
+import betterquesting.questing.QuestInstance;
 import betterquesting.storage.QuestSettings;
+import com.google.common.collect.Maps;
 import com.mojang.realmsclient.gui.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
 import net.minecraft.nbt.NBTTagCompound;
 
 import javax.annotation.Nullable;
@@ -49,11 +49,16 @@ public class PanelButtonQuest extends PanelButtonStorage<Map.Entry<UUID, IQuest>
     {
 		super(rect, id, txt, value);
         this.rect = rect;
-        
         player = Minecraft.getMinecraft().thePlayer;
-        EnumQuestState qState = value == null ? EnumQuestState.LOCKED : value.getValue().getState(player);
+
+		if(value == null) {
+			IQuest dummyQuest = new QuestInstance();
+			value = Maps.immutableEntry(UUID.randomUUID(), dummyQuest);
+		}
+
+        EnumQuestState qState = value.getValue().getState(player);
         IGuiColor txIconCol = null;
-        boolean main = value != null && value.getValue().getProperty(NativeProps.MAIN);
+        boolean main = value.getValue().getProperty(NativeProps.MAIN);
         boolean lock = false;
         
         switch(qState)
@@ -85,10 +90,9 @@ public class PanelButtonQuest extends PanelButtonStorage<Map.Entry<UUID, IQuest>
 
 		IGuiTexture btnTx = new GuiTextureColored(txFrame, txIconCol);
         setTextures(btnTx, btnTx, btnTx);
-        setIcon(new OreDictTexture(1F, value == null ? new BigItemStack(Items.nether_star) : value.getValue().getProperty(NativeProps.ICON), false, true), 4);
+        setIcon(new OreDictTexture(1F, value.getValue().getProperty(NativeProps.ICON), false, true), 4);
         setActive(QuestingAPI.getAPI(ApiReference.SETTINGS).canUserEdit(player) || !lock || BQ_Settings.viewMode);
-		boolean bookmarked = value != null && BookmarkHandler.isBookmarked(value.getKey());
-		setBookmarked(bookmarked);
+		setBookmarked(BookmarkHandler.isBookmarked(value.getKey()));
     }
 
 	@Override
