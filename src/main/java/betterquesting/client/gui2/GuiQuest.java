3d376e3e768aafdc1945df949de21fcf233e3cdc
alppp <turcu.dan991@gmail.com>
2026-04-21 22:37:00 +0300

    strip extended format codes in quest export, text box, and description

--- src/main/java/betterquesting/client/gui2/GuiQuest.java
+++ src/main/java/betterquesting/client/gui2/GuiQuest.java
@@ -68,7 +68,7 @@ public class GuiQuest extends GuiScreenCanvas implements IPEventListener, INeeds
     private static final Map<UUID, ScrollPosition> scrollsPositions = new HashMap<>();
     private static final Pattern img = Pattern.compile("\\[img height=([1-9]\\d*)] *(.*?:.*?) *\\[/img]");
     private static final Pattern DESCRIPTION_FORMATING_REMOVER = Pattern.compile(
-        "§[0-9a-f|k|n|m|o|l|r]|\\[(url|warn|note|quest)]|\\[\\/\1\\]|\\[img.*?\\]|\\[\\/(url|warn|note|quest|img)\\]");
+        "§[0-9a-fk-orxgqzv]|\\[(url|warn|note|quest)]|\\[\\/\1\\]|\\[img.*?\\]|\\[\\/(url|warn|note|quest|img)\\]");
     private ScrollPosition scrollPosition;
 
     public static class ScrollPosition {
