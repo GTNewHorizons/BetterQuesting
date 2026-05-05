224f1b9673e0cddb0da2b8a51e591d992f32d9f1
tiffit <10173431+tiffit@users.noreply.github.com>
2026-05-04 21:58:09 -0700

    Added items properly cycling and popup for all valid items

--- src/main/java/bq_standard/client/gui/panels/content/PanelInteractiveItemSlot.java
+++ src/main/java/bq_standard/client/gui/panels/content/PanelInteractiveItemSlot.java
@@ -1,13 +1,20 @@
 package bq_standard.client.gui.panels.content;
 
+import java.util.List;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.PositionedSoundRecord;
+import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
+import com.mojang.realmsclient.gui.ChatFormatting;
+
 import betterquesting.api.utils.BigItemStack;
+import betterquesting.api2.client.gui.SceneController;
 import betterquesting.api2.client.gui.misc.IGuiRect;
 import betterquesting.api2.client.gui.panels.content.PanelItemSlot;
+import betterquesting.api2.client.gui.popups.PopItemList;
 import codechicken.nei.api.ShortcutInputHandler;
 
 /**
@@ -19,11 +26,14 @@ import codechicken.nei.api.ShortcutInputHandler;
 public class PanelInteractiveItemSlot extends PanelItemSlot {
 
     private boolean isMouseHovered;
+    private final boolean popupVariants;
 
     private static final ResourceLocation CLICK_SND = new ResourceLocation("gui.button.press");
 
-    public PanelInteractiveItemSlot(IGuiRect rect, int id, BigItemStack value, boolean showCount, boolean oreDict) {
+    public PanelInteractiveItemSlot(IGuiRect rect, int id, BigItemStack value, boolean showCount, boolean oreDict,
+        boolean popupVariants) {
         super(rect, id, value, showCount, oreDict);
+        this.popupVariants = popupVariants;
     }
 
     @Override
@@ -41,7 +51,10 @@ public class PanelInteractiveItemSlot extends PanelItemSlot {
     @Override
     public boolean onMouseClick(int mx, int my, int click) {
         if (isMouseHovered && getCallback() == null) {
-            if (ShortcutInputHandler.handleMouseClick(getBaseStackOfSameSize())) {
+            if (popupVariants && GuiContainer.isShiftKeyDown() && SceneController.getActiveScene() != null) {
+                SceneController.getActiveScene()
+                    .openPopup(new PopItemList("Valid Items", oreVariants));
+            } else if (ShortcutInputHandler.handleMouseClick(getBaseStackOfSameSize())) {
                 playClickSound();
                 return true;
             }
@@ -69,5 +82,14 @@ public class PanelInteractiveItemSlot extends PanelItemSlot {
         Minecraft.getMinecraft()
             .getSoundHandler()
             .playSound(PositionedSoundRecord.func_147674_a(CLICK_SND, 1.0F));
+    }
+
+    @Override
+    public List<String> getTooltip(int mx, int my) {
+        List<String> list = super.getTooltip(mx, my);
+        if (list != null && popupVariants) {
+            list.add(ChatFormatting.GRAY + ChatFormatting.ITALIC.toString() + "Shift+click to see valid items");
+        }
+        return list;
     }
 }
