6857771ae049b37c2e968dad902e3f4d7b8ab1d2
danyadev <danyadev@mail.ru>
2026-05-09 16:45:27 +0900

    NetPartyAction cleanup

--- src/main/java/betterquesting/network/handlers/NetPartyAction.java
+++ src/main/java/betterquesting/network/handlers/NetPartyAction.java
@@ -8,6 +8,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.Constants;
 
 import org.apache.logging.log4j.Level;
 
@@ -34,6 +35,20 @@ public class NetPartyAction {
 
     private static final ResourceLocation ID_NAME = new ResourceLocation("betterquesting:party_action");
 
+    private static final int ACTION_CREATE = 0;
+    private static final int ACTION_DELETE = 1;
+    private static final int ACTION_EDIT = 2;
+    private static final int ACTION_INVITE = 3;
+    private static final int ACTION_ACCEPT_INVITE = 4;
+    private static final int ACTION_KICK = 5;
+
+    private static final String TAG_ACTION = "action";
+    private static final String TAG_PARTY_ID = "partyID";
+    private static final String TAG_PARTY_NAME = "name";
+    private static final String TAG_PARTY = "data";
+    private static final String TAG_INVITE_USERNAME = "username";
+    private static final String TAG_INVITE_EXPIRY_TIME = "expiry";
+
     public static void registerHandler() {
         PacketTypeRegistry.INSTANCE.registerServerHandler(ID_NAME, NetPartyAction::onServer);
 
@@ -48,73 +63,44 @@ public class NetPartyAction {
     }
 
     private static void onServer(Tuple2<NBTTagCompound, EntityPlayerMP> message) {
+        NBTTagCompound payload = message.getFirst();
         EntityPlayerMP sender = message.getSecond();
 
-        int action = !message.getFirst()
-            .hasKey("action", 99) ? -1
-                : message.getFirst()
-                    .getInteger("action");
-        int partyID = !message.getFirst()
-            .hasKey("partyID", 99) ? -1
-                : message.getFirst()
-                    .getInteger("partyID");
+        int action = payload.hasKey(TAG_ACTION, Constants.NBT.TAG_ANY_NUMERIC) ? payload.getInteger(TAG_ACTION) : -1;
+        int partyID = payload.hasKey(TAG_PARTY_ID, Constants.NBT.TAG_ANY_NUMERIC) ? payload.getInteger(TAG_PARTY_ID)
+            : -1;
         IParty party = PartyManager.INSTANCE.getValue(partyID);
-        int permission = party == null ? 0 : checkPermission(QuestingAPI.getQuestingUUID(sender), party);
+        int senderPermission = party == null ? 0 : getPermission(QuestingAPI.getQuestingUUID(sender), party);
 
         switch (action) {
-            case 0: {
-                createParty(
-                    sender,
-                    message.getFirst()
-                        .getString("name"));
-                break;
+            case ACTION_CREATE -> createParty(sender, payload.getString(TAG_PARTY_NAME));
+            case ACTION_DELETE -> {
+                if (senderPermission >= 3) {
+                    deleteParty(partyID);
+                }
             }
-            case 1: {
-                if (permission < 3) break;
-                deleteParty(partyID);
-                break;
+            case ACTION_EDIT -> {
+                if (senderPermission >= 2) {
+                    editParty(partyID, party, payload.getCompoundTag(TAG_PARTY));
+                }
             }
-            case 2: {
-                if (permission < 2) break;
-                editParty(
-                    partyID,
-                    party,
-                    message.getFirst()
-                        .getCompoundTag("data"));
-                break;
+            case ACTION_INVITE -> {
+                if (senderPermission >= 2) {
+                    inviteUser(
+                        partyID,
+                        payload.getString(TAG_INVITE_USERNAME),
+                        payload.getLong(TAG_INVITE_EXPIRY_TIME));
+                }
             }
-            case 3: {
-                if (permission < 2) break;
-                inviteUser(
-                    partyID,
-                    message.getFirst()
-                        .getString("username"),
-                    message.getFirst()
-                        .getLong("expiry"));
-                break;
-            }
-            case 4: {
-                acceptInvite(partyID, sender); // Probably the only thing an OP can't force
-                break;
-            }
-            case 5: {
-                kickUser(
-                    partyID,
-                    sender,
-                    party,
-                    message.getFirst()
-                        .getString("username"),
-                    permission);
-                break;
-            }
-            default: {
-                BetterQuesting.logger.log(
-                    Level.ERROR,
-                    "Invalid party action '" + action
-                        + "'. Full payload:\n"
-                        + message.getFirst()
-                            .toString());
-            }
+            case ACTION_ACCEPT_INVITE -> acceptInvite(partyID, sender); // Probably the only thing an OP can't force
+            case ACTION_KICK -> kickUser(
+                partyID,
+                sender,
+                party,
+                payload.getString(TAG_INVITE_USERNAME),
+                senderPermission);
+            default -> BetterQuesting.logger
+                .log(Level.ERROR, "Invalid party action '{}'. Full payload:\n{}", action, payload);
         }
     }
 
@@ -135,35 +121,33 @@ public class NetPartyAction {
         PartyInvitations.INSTANCE.purgeInvites(partyID);
 
         NBTTagCompound payload = new NBTTagCompound();
-        payload.setInteger("action", 1);
-        payload.setInteger("partyID", partyID);
-        PacketSender.INSTANCE.sendToAll(new QuestingPacket(ID_NAME, payload)); // Invites need to be purged from
-                                                                               // everyone
+        payload.setInteger(TAG_ACTION, ACTION_DELETE);
+        payload.setInteger(TAG_PARTY_ID, partyID);
+        // Invites need to be purged from everyone
+        PacketSender.INSTANCE.sendToAll(new QuestingPacket(ID_NAME, payload));
     }
 
-    private static void editParty(int partyID, IParty party, NBTTagCompound settings) {
-        party.readProperties(settings);
+    private static void editParty(int partyID, IParty party, NBTTagCompound partyNBT) {
+        party.readProperties(partyNBT);
         NetPartySync.quickSync(partyID);
     }
 
     private static void inviteUser(int partyID, String username, long expiry) {
-        UUID uuid = null;
         MinecraftServer server = FMLCommonHandler.instance()
             .getMinecraftServerInstance();
         EntityPlayerMP player = server.getConfigurationManager()
             .func_152612_a(username);
-        if (player != null) uuid = QuestingAPI.getQuestingUUID(player);
-        if (uuid == null) uuid = NameCache.INSTANCE.getUUID(username);
-        if (uuid != null) {
-            PartyInvitations.INSTANCE.postInvite(uuid, partyID, expiry);
-            if (player != null) {
-                NetPartySync.sendSync(new EntityPlayerMP[] { player }, new int[] { partyID });
-                NetInviteSync.sendSync(player);
-            }
-        } else {
-            BetterQuesting.logger.error("Unable to identify " + username + " to invite to party " + partyID); // No idea
-                                                                                                              // who
-                                                                                                              // this is
+
+        UUID playerId = player != null ? QuestingAPI.getQuestingUUID(player) : NameCache.INSTANCE.getUUID(username);
+        if (playerId == null) {
+            BetterQuesting.logger.error("Unable to identify {} to invite to party {}", username, partyID);
+            return;
+        }
+
+        PartyInvitations.INSTANCE.postInvite(playerId, partyID, expiry);
+        if (player != null) {
+            NetPartySync.sendSync(new EntityPlayerMP[] { player }, new int[] { partyID });
+            NetInviteSync.sendSync(player);
         }
     }
 
@@ -171,128 +155,108 @@ public class NetPartyAction {
         UUID playerID = QuestingAPI.getQuestingUUID(sender);
         DBEntry<IParty> party = PartyManager.INSTANCE.getParty(playerID);
         if (party != null) return;
+
         if (PartyInvitations.INSTANCE.acceptInvite(playerID, partyID)) {
             NetPartySync.quickSync(partyID);
             NetNameSync.quickSync(sender, partyID);
         } else {
-            BetterQuesting.logger.error("Invalid invite for " + sender.getCommandSenderName() + " to party " + partyID);
+            BetterQuesting.logger.error("Invalid invite for {} to party {}", sender.getCommandSenderName(), partyID);
         }
         NetInviteSync.sendSync(sender);
     }
 
-    private static void kickUser(int partyID, EntityPlayerMP sender, IParty party, String username, int permission) // Is
-                                                                                                                    // also
-                                                                                                                    // the
-                                                                                                                    // leave
-                                                                                                                    // action
-                                                                                                                    // (self
-                                                                                                                    // kick
-                                                                                                                    // if
-                                                                                                                    // you
-                                                                                                                    // will)
-    {
+    // It's also the leave action (self kick if you will)
+    private static void kickUser(int partyId, EntityPlayerMP sender, IParty party, String username,
+        int senderPermission) {
         if (party == null) {
-            BetterQuesting.logger.error("Tried to kick a player from a non-existant party (" + partyID + ")");
+            BetterQuesting.logger.error("Tried to kick a player from a non-existent party ({})", partyId);
             return;
         }
 
-        UUID uuid = null;
         MinecraftServer server = FMLCommonHandler.instance()
             .getMinecraftServerInstance();
-        EntityPlayerMP player = server.getConfigurationManager()
+        EntityPlayerMP target = server.getConfigurationManager()
             .func_152612_a(username);
-        if (player != null) uuid = QuestingAPI.getQuestingUUID(player);
-        if (uuid == null) uuid = NameCache.INSTANCE.getUUID(username);
-        if (uuid == null) {
-            BetterQuesting.logger.error("Unable to identify " + username + " to remove them from party " + partyID);
-            return; // No idea who this is
+
+        UUID targetId = target != null ? QuestingAPI.getQuestingUUID(target) : NameCache.INSTANCE.getUUID(username);
+        if (targetId == null) {
+            BetterQuesting.logger.error("Unable to identify {} to remove them from party {}", username, partyId);
+            return;
         }
 
-        if (uuid.equals(QuestingAPI.getQuestingUUID(sender)) || checkPermission(uuid, party) < permission) // For future
-                                                                                                           // reference,
-                                                                                                           // this is
-                                                                                                           // checking
-                                                                                                           // the target
-                                                                                                           // has a
-                                                                                                           // permission
-                                                                                                           // lower than
-                                                                                                           // the sender
-        {
-            // Even if the kick isn't confirmed we still need to tell the clients incase of desync
-            if (party.getStatus(uuid) != null) party.kickUser(uuid);
+        boolean isLeave = targetId.equals(QuestingAPI.getQuestingUUID(sender));
+        boolean senderCanKickTarget = getPermission(targetId, party) < senderPermission;
+        if (!isLeave && !senderCanKickTarget) {
+            BetterQuesting.logger.error("Insufficient permissions to kick {} from party {}", username, partyId);
+            return;
+        }
 
-            if (party.getMembers()
-                .size() > 0) {
-                NetPartySync.quickSync(partyID);
-                if (player != null) {
-                    NBTTagCompound payload = new NBTTagCompound();
-                    payload.setInteger("action", 5);
-                    payload.setInteger("partyID", partyID);
-                    PacketSender.INSTANCE.sendToPlayers(new QuestingPacket(ID_NAME, payload), player);
-                }
-            } else // No more members. Delete the party
-            {
-                PartyManager.INSTANCE.removeID(partyID);
-                PartyInvitations.INSTANCE.purgeInvites(partyID);
+        // Even if the kick isn't confirmed we still need to tell the clients in case of desync
+        if (party.getStatus(targetId) != null) {
+            party.kickUser(targetId);
+        }
 
+        if (!party.getMembers()
+            .isEmpty()) {
+            NetPartySync.quickSync(partyId);
+            if (target != null) {
                 NBTTagCompound payload = new NBTTagCompound();
-                payload.setInteger("action", 1);
-                payload.setInteger("partyID", partyID);
-                PacketSender.INSTANCE.sendToAll(new QuestingPacket(ID_NAME, payload)); // Invites need to be purged from
-                                                                                       // everyone
+                payload.setInteger(TAG_ACTION, ACTION_KICK);
+                payload.setInteger(TAG_PARTY_ID, partyId);
+                PacketSender.INSTANCE.sendToPlayers(new QuestingPacket(ID_NAME, payload), target);
             }
         } else {
-            BetterQuesting.logger.error("Insufficient permissions to kick " + username + " from party " + partyID);
+            PartyManager.INSTANCE.removeID(partyId);
+            PartyInvitations.INSTANCE.purgeInvites(partyId);
+
+            NBTTagCompound payload = new NBTTagCompound();
+            payload.setInteger(TAG_ACTION, ACTION_DELETE);
+            payload.setInteger(TAG_PARTY_ID, partyId);
+            // Invites need to be purged from everyone
+            PacketSender.INSTANCE.sendToAll(new QuestingPacket(ID_NAME, payload));
         }
     }
 
-    private static int checkPermission(UUID playerID, IParty party) {
+    private static int getPermission(UUID playerID, IParty party) {
         MinecraftServer server = FMLCommonHandler.instance()
             .getMinecraftServerInstance();
-        if (server != null) {
-            EntityPlayerMP player = null;
-            for (Object o : server.getConfigurationManager().playerEntityList) {
-                if (((EntityPlayerMP) o).getGameProfile()
-                    .getId()
-                    .equals(playerID)) {
-                    player = (EntityPlayerMP) o;
-                }
+
+        EntityPlayerMP player = null;
+        for (EntityPlayerMP playerMP : server.getConfigurationManager().playerEntityList) {
+            if (playerMP.getGameProfile()
+                .getId()
+                .equals(playerID)) {
+                player = playerMP;
             }
-            if (player != null && server.getConfigurationManager()
-                .func_152596_g(player.getGameProfile())) return 4; // Can kick owners or force invites without needing
-                                                                   // to be a member of the party
         }
+
+        // OPs can kick owners or force invites without needing to be a member of the party
+        if (player != null && server.getConfigurationManager()
+            .func_152596_g(player.getGameProfile())) return 4;
 
         EnumPartyStatus status = party.getStatus(playerID);
-        if (status == null) return 0; // Only OPs can edit parties they aren't a member of
+        if (status == null) return 0;
 
-        switch (status) {
-            case MEMBER:
-                return 1;
-            case ADMIN:
-                return 2;
-            case OWNER:
-                return 3;
-            default:
-                return 0;
-        }
+        return switch (status) {
+            case MEMBER -> 1;
+            case ADMIN -> 2;
+            case OWNER -> 3;
+        };
     }
 
     @SideOnly(Side.CLIENT)
     private static void onClient(NBTTagCompound message) {
-        int action = !message.hasKey("action", 99) ? -1 : message.getInteger("action");
-        int partyID = !message.hasKey("partyID", 99) ? -1 : message.getInteger("partyID");
+        int action = message.hasKey(TAG_ACTION, Constants.NBT.TAG_ANY_NUMERIC) ? message.getInteger(TAG_ACTION) : -1;
+        int partyID = message.hasKey(TAG_PARTY_ID, Constants.NBT.TAG_ANY_NUMERIC) ? message.getInteger(TAG_PARTY_ID)
+            : -1;
 
         switch (action) {
-            case 1: // Delete
-            {
+            case ACTION_DELETE -> {
                 PartyManager.INSTANCE.removeID(partyID);
                 PartyInvitations.INSTANCE.purgeInvites(partyID);
                 MinecraftForge.EVENT_BUS.post(new DatabaseEvent.Update(DBType.PARTY));
-                break;
             }
-            case 5: // Kicked
-            {
+            case ACTION_KICK -> {
                 IParty party = PartyManager.INSTANCE.getValue(partyID);
                 if (party != null) {
                     party.kickUser(QuestingAPI.getQuestingUUID(Minecraft.getMinecraft().thePlayer));
