GTNewHorizons/BetterQuesting

commit 57e523aca61a7e2f2d4e1744b6ce6551c3a294ad

Huge performance improvements (#255)

src/main/java/betterquesting/api2/client/gui/panels/lists/CanvasScrolling.java
@@ -122,13 +122,13 @@ public class CanvasScrolling implements IGuiCanvas {
     public void setScrollX(int sx) {
         if (scrollBounds.getWidth() <= 0) return;
         scrollX.writeValueRaw((sx - scrollBounds.getX()) / (float) scrollBounds.getWidth());
-        lsx = this.getScrollX();
+        updatePanelScroll();
     }
 
     public void setScrollY(int sy) {
         if (scrollBounds.getHeight() <= 0) return;
         scrollY.writeValueRaw((sy - scrollBounds.getY()) / (float) scrollBounds.getHeight());
-        lsy = this.getScrollY();
+        updatePanelScroll();
     }
 
     public void setZoom(float z) {
@@ -167,6 +167,8 @@ public class CanvasScrolling implements IGuiCanvas {
     @Override
     public void drawPanel(int mx, int my, float partialTick) {
         float zs = zoomScale.readValue();
+        int viewportWidth = (int) Math.ceil(transform.getWidth() / zs);
+        int viewportHeight = (int) Math.ceil(transform.getHeight() / zs);
 
         int tx = transform.getX();
         int ty = transform.getY();
@@ -245,11 +247,12 @@ public class CanvasScrolling implements IGuiCanvas {
 
             }
 
-            lsx = getScrollX();
-            lsy = getScrollY();
+            this.updatePanelScroll();
             lsz = zs;
 
-        } else if (lsx != getScrollX() || lsy != getScrollY()) // We can skip this if the above case ran
+        } else if (lsx != getScrollX() || lsy != getScrollY()
+            || scrollWindow.w != viewportWidth
+            || scrollWindow.h != viewportHeight) // We can skip this if the above case ran
         {
             this.updatePanelScroll();
         }
@@ -265,7 +268,7 @@ public class CanvasScrolling implements IGuiCanvas {
         int smy = (int) ((my - ty) / zs) + lsy;
 
         for (IGuiPanel panel : getVisiblePanels()) {
-            if (panel.isEnabled()) {
+            if (panel.isEnabled() && shouldDrawPanel(panel)) {
                 panel.drawPanel(smx, smy, partialTick);
             }
         }
@@ -601,7 +604,15 @@ public class CanvasScrolling implements IGuiCanvas {
         refreshScrollBounds();
     }
 
-    private List<IGuiPanel> getVisiblePanels() {
+    protected boolean shouldDrawPanel(IGuiPanel panel) {
+        return true;
+    }
+
+    protected boolean isBlockingEnabled() {
+        return useBlocking;
+    }
+
+    protected List<IGuiPanel> getVisiblePanels() {
         return useBlocking ? cullingManager.getVisiblePanels() : guiPanels;
     }
 }

src/main/java/betterquesting/api2/client/gui/resources/textures/SimpleTexture.java
@@ -29,6 +29,10 @@ public class SimpleTexture implements IGuiTexture {
         return this;
     }
 
+    public boolean isMaintainingAspect() {
+        return maintainAspect;
+    }
+
     @Override
     public void drawTexture(int x, int y, int width, int height, float zLevel, float partialTick) {
         drawTexture(x, y, width, height, zLevel, partialTick, defColor);

src/main/java/betterquesting/api2/client/gui/resources/textures/SlicedTexture.java
@@ -2,6 +2,7 @@ package betterquesting.api2.client.gui.resources.textures;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.ResourceLocation;
 
 import org.lwjgl.opengl.GL11;
@@ -18,6 +19,9 @@ import cpw.mods.fml.client.config.GuiUtils;
 
 public class SlicedTexture implements IGuiTexture {
 
+    // The 4096th textured quad forces Tessellator past its retained 0x20000-int buffer.
+    private static final int MAX_BATCH_QUADS = 4095;
+    private static final float TEXEL = 1F / 256F;
     private static final IGuiColor defColor = new GuiColorStatic(255, 255, 255, 255);
 
     private final ResourceLocation texture;
@@ -79,126 +83,144 @@ public class SlicedTexture implements IGuiTexture {
             int iv = texBounds.getY() + texBorder.getTop();
             int iw = texBounds.getWidth() - texBorder.getLeft() - texBorder.getRight();
             int ih = texBounds.getHeight() - texBorder.getTop() - texBorder.getBottom();
-
-            float sx = (float) (w - (texBounds.getWidth() - iw)) / (float) iw;
-            float sy = (float) (h - (texBounds.getHeight() - ih)) / (float) ih;
+            if (iw < 0 || ih < 0) {
+                GL11.glPopMatrix();
+                return;
+            }
+            int sw = w - texBorder.getLeft() - texBorder.getRight();
+            int sh = h - texBorder.getTop() - texBorder.getBottom();
 
             Minecraft.getMinecraft().renderEngine.bindTexture(texture);
+            Tessellator tessellator = Tessellator.instance;
+            tessellator.startDrawingQuads();
+            int batchSize = 0;
 
             // TOP LEFT
-            GL11.glPushMatrix();
-            GL11.glTranslatef(dx, dy, 0F);
-            GuiUtils.drawTexturedModalRect(
-                0,
-                0,
+            batchSize = addTexturedQuad(
+                tessellator,
+                batchSize,
+                dx,
+                dy,
+                texBorder.getLeft(),
+                texBorder.getTop(),
                 texBounds.getX(),
                 texBounds.getY(),
                 texBorder.getLeft(),
                 texBorder.getTop(),
                 zLevel);
-            GL11.glPopMatrix();
 
             // TOP SIDE
-            GL11.glPushMatrix();
-            GL11.glTranslatef(dx + texBorder.getLeft(), dy, 0F);
-            GL11.glScalef(sx, 1F, 1F);
-            GuiUtils.drawTexturedModalRect(
-                0,
-                0,
+            batchSize = addTexturedQuad(
+                tessellator,
+                batchSize,
+                dx + texBorder.getLeft(),
+                dy,
+                sw,
+                texBorder.getTop(),
                 texBounds.getX() + texBorder.getLeft(),
                 texBounds.getY(),
                 iw,
                 texBorder.getTop(),
                 zLevel);
-            GL11.glPopMatrix();
 
             // TOP RIGHT
-            GL11.glPushMatrix();
-            GL11.glTranslatef(dx + w - texBorder.getRight(), dy, 0F);
-            GuiUtils.drawTexturedModalRect(
-                0,
-                0,
+            batchSize = addTexturedQuad(
+                tessellator,
+                batchSize,
+                dx + w - texBorder.getRight(),
+                dy,
+                texBorder.getRight(),
+                texBorder.getTop(),
                 texBounds.getX() + texBorder.getLeft() + iw,
                 texBounds.getY(),
                 texBorder.getRight(),
                 texBorder.getTop(),
                 zLevel);
-            GL11.glPopMatrix();
 
             // LEFT SIDE
-            GL11.glPushMatrix();
-            GL11.glTranslatef(dx, dy + texBorder.getTop(), 0F);
-            GL11.glScalef(1F, sy, 1F);
-            GuiUtils.drawTexturedModalRect(
-                0,
-                0,
+            batchSize = addTexturedQuad(
+                tessellator,
+                batchSize,
+                dx,
+                dy + texBorder.getTop(),
+                texBorder.getLeft(),
+                sh,
                 texBounds.getX(),
                 texBounds.getY() + texBorder.getTop(),
                 texBorder.getLeft(),
                 ih,
                 zLevel);
-            GL11.glPopMatrix();
 
             // MIDDLE
-            GL11.glPushMatrix();
-            GL11.glTranslatef(dx + texBorder.getLeft(), dy + texBorder.getTop(), 0F);
-            GL11.glScalef(sx, sy, 1F);
-            GuiUtils.drawTexturedModalRect(0, 0, iu, iv, iw, ih, zLevel);
-            GL11.glPopMatrix();
+            batchSize = addTexturedQuad(
+                tessellator,
+                batchSize,
+                dx + texBorder.getLeft(),
+                dy + texBorder.getTop(),
+                sw,
+                sh,
+                iu,
+                iv,
+                iw,
+                ih,
+                zLevel);
 
             // RIGHT SIDE
-            GL11.glPushMatrix();
-            GL11.glTranslatef(dx + w - texBorder.getRight(), dy + texBorder.getTop(), 0F);
-            GL11.glScalef(1F, sy, 1F);
-            GuiUtils.drawTexturedModalRect(
-                0,
-                0,
+            batchSize = addTexturedQuad(
+                tessellator,
+                batchSize,
+                dx + w - texBorder.getRight(),
+                dy + texBorder.getTop(),
+                texBorder.getRight(),
+                sh,
                 texBounds.getX() + texBorder.getLeft() + iw,
                 texBounds.getY() + texBorder.getTop(),
                 texBorder.getRight(),
                 ih,
                 zLevel);
-            GL11.glPopMatrix();
 
             // BOTTOM LEFT
-            GL11.glPushMatrix();
-            GL11.glTranslatef(dx, dy + h - texBorder.getBottom(), 0F);
-            GuiUtils.drawTexturedModalRect(
-                0,
-                0,
+            batchSize = addTexturedQuad(
+                tessellator,
+                batchSize,
+                dx,
+                dy + h - texBorder.getBottom(),
+                texBorder.getLeft(),
+                texBorder.getBottom(),
                 texBounds.getX(),
                 texBounds.getY() + texBorder.getTop() + ih,
                 texBorder.getLeft(),
                 texBorder.getBottom(),
                 zLevel);
-            GL11.glPopMatrix();
 
             // BOTTOM SIDE
-            GL11.glPushMatrix();
-            GL11.glTranslatef(dx + texBorder.getLeft(), dy + h - texBorder.getBottom(), 0F);
-            GL11.glScalef(sx, 1F, 1F);
-            GuiUtils.drawTexturedModalRect(
-                0,
-                0,
+            batchSize = addTexturedQuad(
+                tessellator,
+                batchSize,
+                dx + texBorder.getLeft(),
+                dy + h - texBorder.getBottom(),
+                sw,
+                texBorder.getBottom(),
                 texBounds.getX() + texBorder.getLeft(),
                 texBounds.getY() + texBorder.getTop() + ih,
                 iw,
                 texBorder.getBottom(),
                 zLevel);
-            GL11.glPopMatrix();
 
             // BOTTOM RIGHT
-            GL11.glPushMatrix();
-            GL11.glTranslatef(dx + w - texBorder.getRight(), dy + h - texBorder.getBottom(), 0F);
-            GuiUtils.drawTexturedModalRect(
-                0,
-                0,
+            addTexturedQuad(
+                tessellator,
+                batchSize,
+                dx + w - texBorder.getRight(),
+                dy + h - texBorder.getBottom(),
+                texBorder.getRight(),
+                texBorder.getBottom(),
                 texBounds.getX() + texBorder.getLeft() + iw,
                 texBounds.getY() + texBorder.getTop() + ih,
                 texBorder.getRight(),
                 texBorder.getBottom(),
                 zLevel);
-            GL11.glPopMatrix();
+            tessellator.draw();
         } else {
             float sx = (float) w / (float) texBounds.getWidth();
             float sy = (float) h / (float) texBounds.getHeight();
@@ -234,6 +256,10 @@ public class SlicedTexture implements IGuiTexture {
 
     public GuiPadding getBorder() {
         return this.texBorder;
+    }
+
+    public SliceMode getSliceMode() {
+        return sliceMode;
     }
 
     /**
@@ -291,12 +317,17 @@ public class SlicedTexture implements IGuiTexture {
         int remainderWidth = canvasWidth % fillerWidth;
         int yPasses = canvasHeight / fillerHeight;
         int remainderHeight = canvasHeight % fillerHeight;
+        Tessellator tessellator = Tessellator.instance;
+        tessellator.startDrawingQuads();
+        int batchSize = 0;
 
         // Draw Border
         // Top Left
-        GuiUtils.drawTexturedModalRect(x, y, u, v, leftBorder, topBorder, zLevel);
+        batchSize = addTexturedTile(tessellator, batchSize, x, y, u, v, leftBorder, topBorder, zLevel);
         // Top Right
-        GuiUtils.drawTexturedModalRect(
+        batchSize = addTexturedTile(
+            tessellator,
+            batchSize,
             x + leftBorder + canvasWidth,
             y,
             u + leftBorder + fillerWidth,
@@ -305,7 +336,9 @@ public class SlicedTexture implements IGuiTexture {
             topBorder,
             zLevel);
         // Bottom Left
-        GuiUtils.drawTexturedModalRect(
+        batchSize = addTexturedTile(
+            tessellator,
+            batchSize,
             x,
             y + topBorder + canvasHeight,
             u,
@@ -314,7 +347,9 @@ public class SlicedTexture implements IGuiTexture {
             bottomBorder,
             zLevel);
         // Bottom Right
-        GuiUtils.drawTexturedModalRect(
+        batchSize = addTexturedTile(
+            tessellator,
+            batchSize,
             x + leftBorder + canvasWidth,
             y + topBorder + canvasHeight,
             u + leftBorder + fillerWidth,
@@ -324,57 +359,98 @@ public class SlicedTexture implements IGuiTexture {
             zLevel);
 
         for (int i = 0; i < xPasses + (remainderWidth > 0 ? 1 : 0); i++) {
+            int drawWidth = i == xPasses ? remainderWidth : fillerWidth;
             // Top Border
-            GuiUtils.drawTexturedModalRect(
+            batchSize = addTexturedTile(
+                tessellator,
+                batchSize,
                 x + leftBorder + (i * fillerWidth),
                 y,
                 u + leftBorder,
                 v,
-                (i == xPasses ? remainderWidth : fillerWidth),
+                drawWidth,
                 topBorder,
                 zLevel);
             // Bottom Border
-            GuiUtils.drawTexturedModalRect(
+            batchSize = addTexturedTile(
+                tessellator,
+                batchSize,
                 x + leftBorder + (i * fillerWidth),
                 y + topBorder + canvasHeight,
                 u + leftBorder,
                 v + topBorder + fillerHeight,
-                (i == xPasses ? remainderWidth : fillerWidth),
+                drawWidth,
                 bottomBorder,
                 zLevel);
 
             // Throw in some filler for good measure
-            for (int j = 0; j < yPasses + (remainderHeight > 0 ? 1 : 0); j++) GuiUtils.drawTexturedModalRect(
-                x + leftBorder + (i * fillerWidth),
-                y + topBorder + (j * fillerHeight),
-                u + leftBorder,
-                v + topBorder,
-                (i == xPasses ? remainderWidth : fillerWidth),
-                (j == yPasses ? remainderHeight : fillerHeight),
-                zLevel);
+            for (int j = 0; j < yPasses + (remainderHeight > 0 ? 1 : 0); j++) {
+                int drawHeight = j == yPasses ? remainderHeight : fillerHeight;
+                batchSize = addTexturedTile(
+                    tessellator,
+                    batchSize,
+                    x + leftBorder + (i * fillerWidth),
+                    y + topBorder + (j * fillerHeight),
+                    u + leftBorder,
+                    v + topBorder,
+                    drawWidth,
+                    drawHeight,
+                    zLevel);
+            }
         }
 
         // Side Borders
         for (int j = 0; j < yPasses + (remainderHeight > 0 ? 1 : 0); j++) {
+            int drawHeight = j == yPasses ? remainderHeight : fillerHeight;
             // Left Border
-            GuiUtils.drawTexturedModalRect(
+            batchSize = addTexturedTile(
+                tessellator,
+                batchSize,
                 x,
                 y + topBorder + (j * fillerHeight),
                 u,
                 v + topBorder,
                 leftBorder,
-                (j == yPasses ? remainderHeight : fillerHeight),
+                drawHeight,
                 zLevel);
             // Right Border
-            GuiUtils.drawTexturedModalRect(
+            batchSize = addTexturedTile(
+                tessellator,
+                batchSize,
                 x + leftBorder + canvasWidth,
                 y + topBorder + (j * fillerHeight),
                 u + leftBorder + fillerWidth,
                 v + topBorder,
                 rightBorder,
-                (j == yPasses ? remainderHeight : fillerHeight),
+                drawHeight,
                 zLevel);
         }
+        tessellator.draw();
+    }
+
+    private static int addTexturedTile(Tessellator tessellator, int batchSize, int x, int y, int u, int v, int width,
+        int height, float zLevel) {
+        return addTexturedQuad(tessellator, batchSize, x, y, width, height, u, v, width, height, zLevel);
+    }
+
+    private static int addTexturedQuad(Tessellator tessellator, int batchSize, int x, int y, int width, int height,
+        int u, int v, int textureWidth, int textureHeight, float zLevel) {
+        if (width <= 0 || height <= 0 || textureWidth <= 0 || textureHeight <= 0) return batchSize;
+        if (batchSize >= MAX_BATCH_QUADS) {
+            tessellator.draw();
+            tessellator.startDrawingQuads();
+            batchSize = 0;
+        }
+
+        float minU = u * TEXEL;
+        float minV = v * TEXEL;
+        float maxU = (u + textureWidth) * TEXEL;
+        float maxV = (v + textureHeight) * TEXEL;
+        tessellator.addVertexWithUV(x, y + height, zLevel, minU, maxV);
+        tessellator.addVertexWithUV(x + width, y + height, zLevel, maxU, maxV);
+        tessellator.addVertexWithUV(x + width, y, zLevel, maxU, minV);
+        tessellator.addVertexWithUV(x, y, zLevel, minU, minV);
+        return batchSize + 1;
     }
 
     public enum SliceMode {

src/main/java/betterquesting/api/storage/BQ_Settings.java
@@ -40,6 +40,8 @@ public class BQ_Settings {
 
     public static float zoomSpeed = 1.25f;
     public static float zoomTimeInMs = 100f;
+    public static int questIconCacheMode = 1;
+    public static int questIconCacheFps = 16;
 
     public static boolean zoomInToCursor = true;
     public static boolean zoomOutToCursor = false;

src/main/java/betterquesting/api2/client/gui/GuiScreenCanvas.java
@@ -29,6 +29,7 @@ import betterquesting.api2.client.gui.misc.GuiRectangle;
 import betterquesting.api2.client.gui.misc.GuiTransform;
 import betterquesting.api2.client.gui.misc.IGuiRect;
 import betterquesting.api2.client.gui.panels.IGuiPanel;
+import betterquesting.api2.client.gui.panels.lists.CanvasQuestLine;
 import betterquesting.api2.client.gui.popups.PopChoice;
 import betterquesting.api2.client.gui.themes.presets.PresetIcon;
 import betterquesting.api2.utils.QuestTranslation;
@@ -136,6 +137,7 @@ public class GuiScreenCanvas extends GuiScreen implements IScene {
         super.onGuiClosed();
 
         Keyboard.enableRepeatEvents(false);
+        CanvasQuestLine.releaseButtonCache();
     }
 
     @Override

src/main/java/betterquesting/api2/client/gui/controls/PanelButtonQuest.java
@@ -96,7 +96,7 @@ public class PanelButtonQuest extends PanelButtonStorage<Map.Entry<UUID, IQuest>
                 txFrame = null;
         }
 
-        IGuiTexture btnTx = new GuiTextureColored(txFrame, txIconCol);
+        IGuiTexture btnTx = new GuiTextureColored(QuestFrameCache.wrap(txFrame), txIconCol);
         setTextures(btnTx, btnTx, btnTx);
         setIcon(
             new OreDictTexture(
@@ -130,6 +130,10 @@ public class PanelButtonQuest extends PanelButtonStorage<Map.Entry<UUID, IQuest>
 
     public void setBookmarked(boolean state) {
         isBookmarked = state;
+    }
+
+    public boolean isBookmarked() {
+        return isBookmarked;
     }
 
     @Override

commit b545ce88e5ea4773c3cdb6baff304de6208a4a27

Fix rendering issues introduced by https://github.com/GTNewHorizons/BetterQuesting/pull/255 (#257)

src/main/java/betterquesting/api2/client/gui/panels/lists/CanvasQuestLine.java
@@ -237,7 +237,7 @@ public class CanvasQuestLine extends CanvasScrolling {
         if (clip) RenderUtils.startScissor(bounds);
         try {
             GL11.glTranslatef(tx - lsx * zs, ty - lsy * zs, 0F);
-            GL11.glScalef(zs, zs, zs);
+            GL11.glScalef(zs, zs, 1F);
             GL11.glDisable(GL11.GL_TEXTURE_2D);
 
             Tessellator tessellator = Tessellator.instance;
@@ -280,11 +280,35 @@ public class CanvasQuestLine extends CanvasScrolling {
         if (clip) RenderUtils.startScissor(bounds);
         try {
             GL11.glTranslatef(tx - lsx * zs, ty - lsy * zs, 0F);
-            GL11.glScalef(zs, zs, zs);
+            GL11.glScalef(zs, zs, 1F);
 
             for (IGuiPanel panel : getVisiblePanels()) {
                 if (panel instanceof PanelButtonQuest && panel.isEnabled()) {
                     panel.drawPanel(smx, smy, partialTick);
+                }
+            }
+
+            if (!clip) {
+                GL11.glPushAttrib(GL11.GL_COLOR_BUFFER_BIT);
+                GL11.glColorMask(false, false, false, true);
+                try {
+                    for (IGuiPanel panel : getVisiblePanels()) {
+                        if (panel instanceof PanelButtonQuest && panel.isEnabled()) {
+                            PanelButtonQuest button = (PanelButtonQuest) panel;
+                            IGuiRect frame = button.getTransform();
+                            if (button.txFrame != null) {
+                                button.txFrame.drawTexture(
+                                    frame.getX(),
+                                    frame.getY(),
+                                    frame.getWidth(),
+                                    frame.getHeight(),
+                                    0F,
+                                    partialTick);
+                            }
+                        }
+                    }
+                } finally {
+                    GL11.glPopAttrib();
                 }
             }
         } finally {
@@ -633,13 +657,14 @@ public class CanvasQuestLine extends CanvasScrolling {
                     cache.framebufferClear();
                 }
                 cache.bindFramebuffer(false);
-                GL11.glDisable(GL11.GL_BLEND);
+                // Preserve alpha coverage from every button layer.
+                GL11.glEnable(GL11.GL_BLEND);
                 GL11.glDisable(GL11.GL_DEPTH_TEST);
                 GL11.glDepthMask(true);
                 OpenGlHelper.glBlendFunc(
                     GL11.GL_SRC_ALPHA,
                     GL11.GL_ONE_MINUS_SRC_ALPHA,
-                    GL11.GL_SRC_ALPHA,
+                    GL11.GL_ONE,
                     GL11.GL_ONE_MINUS_SRC_ALPHA);
                 GL11.glColor4f(1F, 1F, 1F, 1F);
                 drawButtons.run();

src/main/java/betterquesting/api2/client/gui/panels/lists/CanvasScrolling.java
@@ -262,7 +262,7 @@ public class CanvasScrolling implements IGuiCanvas {
         RenderUtils.startScissor(transform);
 
         GL11.glTranslatef(tx - lsx * zs, ty - lsy * zs, 0F);
-        GL11.glScalef(zs, zs, zs);
+        GL11.glScalef(zs, zs, 1F);
 
         int smx = (int) ((mx - tx) / zs) + lsx;
         int smy = (int) ((my - ty) / zs) + lsy;

src/main/java/betterquesting/api2/client/gui/resources/textures/SimpleTexture.java
@@ -62,7 +62,8 @@ public class SimpleTexture implements IGuiTexture {
         color.applyGlColor();
 
         GL11.glEnable(GL11.GL_BLEND);
-        OpenGlHelper.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
+        OpenGlHelper
+            .glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
         Minecraft.getMinecraft().renderEngine.bindTexture(texture);
         GuiUtils.drawTexturedModalRect(

src/main/java/betterquesting/api2/client/gui/resources/textures/SlicedTexture.java
@@ -53,7 +53,8 @@ public class SlicedTexture implements IGuiTexture {
         color.applyGlColor();
 
         GL11.glEnable(GL11.GL_BLEND);
-        OpenGlHelper.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
+        OpenGlHelper
+            .glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
         if (w != width || h != height) {
             dx = 0;
@@ -228,7 +229,8 @@ public class SlicedTexture implements IGuiTexture {
             GL11.glScalef(sx, sy, 1F);
 
             GL11.glEnable(GL11.GL_BLEND);
-            OpenGlHelper.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
+            OpenGlHelper
+                .glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
             Minecraft.getMinecraft().renderEngine.bindTexture(texture);
             GuiUtils.drawTexturedModalRect(
@@ -306,7 +308,8 @@ public class SlicedTexture implements IGuiTexture {
         Minecraft.getMinecraft().renderEngine.bindTexture(res);
 
         GL11.glEnable(GL11.GL_BLEND);
-        OpenGlHelper.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
+        OpenGlHelper
+            .glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
         int fillerWidth = textureWidth - leftBorder - rightBorder;
         int fillerHeight = textureHeight - topBorder - bottomBorder;
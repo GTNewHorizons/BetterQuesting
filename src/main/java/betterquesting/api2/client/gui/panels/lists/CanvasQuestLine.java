b545ce88e5ea4773c3cdb6baff304de6208a4a27
boubou19 <miisterunknown@gmail.com>
2026-09-02 02:23:48 +0200

    Fix rendering issues introduced by https://github.com/GTNewHorizons/BetterQuesting/pull/255 (#257)

--- src/main/java/betterquesting/api2/client/gui/panels/lists/CanvasQuestLine.java
+++ src/main/java/betterquesting/api2/client/gui/panels/lists/CanvasQuestLine.java
@@ -237,7 +237,7 @@ public class CanvasQuestLine extends CanvasScrolling {
         if (clip) RenderUtils.startScissor(bounds);
         try {
             GL11.glTranslatef(tx - lsx * zs, ty - lsy * zs, 0F);
-            GL11.glScalef(zs, zs, zs);
+            GL11.glScalef(zs, zs, 1F);
             GL11.glDisable(GL11.GL_TEXTURE_2D);
 
             Tessellator tessellator = Tessellator.instance;
@@ -280,11 +280,35 @@ public class CanvasQuestLine extends CanvasScrolling {
         if (clip) RenderUtils.startScissor(bounds);
         try {
             GL11.glTranslatef(tx - lsx * zs, ty - lsy * zs, 0F);
-            GL11.glScalef(zs, zs, zs);
+            GL11.glScalef(zs, zs, 1F);
 
             for (IGuiPanel panel : getVisiblePanels()) {
                 if (panel instanceof PanelButtonQuest && panel.isEnabled()) {
                     panel.drawPanel(smx, smy, partialTick);
+                }
+            }
+
+            if (!clip) {
+                GL11.glPushAttrib(GL11.GL_COLOR_BUFFER_BIT);
+                GL11.glColorMask(false, false, false, true);
+                try {
+                    for (IGuiPanel panel : getVisiblePanels()) {
+                        if (panel instanceof PanelButtonQuest && panel.isEnabled()) {
+                            PanelButtonQuest button = (PanelButtonQuest) panel;
+                            IGuiRect frame = button.getTransform();
+                            if (button.txFrame != null) {
+                                button.txFrame.drawTexture(
+                                    frame.getX(),
+                                    frame.getY(),
+                                    frame.getWidth(),
+                                    frame.getHeight(),
+                                    0F,
+                                    partialTick);
+                            }
+                        }
+                    }
+                } finally {
+                    GL11.glPopAttrib();
                 }
             }
         } finally {
@@ -633,13 +657,14 @@ public class CanvasQuestLine extends CanvasScrolling {
                     cache.framebufferClear();
                 }
                 cache.bindFramebuffer(false);
-                GL11.glDisable(GL11.GL_BLEND);
+                // Preserve alpha coverage from every button layer.
+                GL11.glEnable(GL11.GL_BLEND);
                 GL11.glDisable(GL11.GL_DEPTH_TEST);
                 GL11.glDepthMask(true);
                 OpenGlHelper.glBlendFunc(
                     GL11.GL_SRC_ALPHA,
                     GL11.GL_ONE_MINUS_SRC_ALPHA,
-                    GL11.GL_SRC_ALPHA,
+                    GL11.GL_ONE,
                     GL11.GL_ONE_MINUS_SRC_ALPHA);
                 GL11.glColor4f(1F, 1F, 1F, 1F);
                 drawButtons.run();
